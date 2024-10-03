551630af466f10774bef39be94298f59718de07d
ne14-gp <pgj646@gmail.com>
2024-10-03 16:31:46 +0100

    table styles

--- public/env.js
+++ public/env.js
@@ -1,6 +1,6 @@
 window.__env = {
-  apiUrl: 'https://localhost:7096',
-  //apiUrl: 'https://portalapi.goaldiggers.dev',
+  //apiUrl: 'https://localhost:7096',
+  apiUrl: 'https://portalapi.goaldiggers.dev',
   b2cClientId: '3b1ad634-3c6d-4884-834f-ab8b3f5ef1ea',
   b2cSignInId: 'B2C_1_signUpOrSignIn',
   b2cTenantSubdomain: 'ne14auth',
